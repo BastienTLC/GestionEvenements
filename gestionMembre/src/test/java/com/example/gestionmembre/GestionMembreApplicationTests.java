5aa132207cdc92a9e5602180167553ed0f80ff1f
BastienTLC <bastientalec@orange.fr>
2024-02-20 13:57:10 +0100

    initialisation gestion membre

---
+++ gestionMembre/src/test/java/com/example/gestionmembre/GestionMembreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionmembre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionMembreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
