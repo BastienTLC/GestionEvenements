878596955df13f531bdde3392cf607d435f66d48
Youenn Gasc <59448502+Treldyr@users.noreply.github.com>
2024-02-22 17:49:34 +0100

    finish get + ajout du post ( lien du post still inreachable)

--- gestionMessages/src/main/java/MessagesServlet.java
+++ gestionMessages/src/main/java/MessagesServlet.java
@@ -10,6 +10,9 @@ import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 
@@ -24,11 +27,19 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 
 
 public class MessagesServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	
+	// --------------
+	//
+	// DO - GET
+	//
+	// --------------
 	
 	
 	public List<Message> getListeMessagesMongoDB() {
@@ -110,44 +121,107 @@ public class MessagesServlet extends HttpServlet {
 		
 		return messagesClones;
 	}
-	
+		
 	
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String operation = request.getParameter("operation");
-		
-		if ((operation!=null)&&(operation.equals("listeMessageMongoDB"))) {
-			// récupère la liste des sportifs et l'associe ï¿½ la requï¿½te HTTP
-			request.setAttribute("messages", this.getListeMessagesMongoDB());
-			// forwarde la requï¿½te ï¿½ la page JSP
-			getServletConfig().getServletContext().getRequestDispatcher("/afficheMessagesMongoDB.jsp")
-				.forward(request, response);
-		}
-		
-		String idMembre = request.getParameter("idMembre");
-		
+		String idMembre = request.getParameter("userId");
+		String idEvent = request.getParameter("eventId");
+			
 		if (idMembre!=null) {
-			request.setAttribute("messages", this.getMessagesMongoByMembreId(Integer.parseInt(idMembre)));
-			// forwarde la requï¿½te ï¿½ la page JSP
-			getServletConfig().getServletContext().getRequestDispatcher("/afficheMessagesMongoDB.jsp")
-				.forward(request, response);
-		}
-		
-		String idEvent = request.getParameter("idEvent");
-		
-		if (idEvent!=null) {
-			request.setAttribute("messages", this.getMessagesMongoByEventId(Integer.parseInt(idEvent)));
-			// forwarde la requï¿½te ï¿½ la page JSP
-			getServletConfig().getServletContext().getRequestDispatcher("/afficheMessagesMongoDB.jsp")
-				.forward(request, response);
+			List<Message> messagesClones = this.getMessagesMongoByMembreId(Integer.parseInt(idMembre));
+			
+			response.setContentType("application/json; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			String json = "[";
+			for (Message msg : messagesClones) {
+				json += "\"{\"contenu\" : \""+msg.getContenu() + "\", \"idMembre\" : \""+msg.getIdMembre()
+						+ "\", \"idEvent\" : \""+msg.getIdEvent()+"\", \"date\" : \""+msg.getDate()+"\"}";
+			}
+			json += "]";
+			out.write(json);
+			out.close();
+		} else if (idEvent!=null) {
+			List<Message> messagesClones = this.getMessagesMongoByEventId(Integer.parseInt(idEvent));
+			
+			response.setContentType("application/json; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			String json = "[";
+			for (Message msg : messagesClones) {
+				json += "\"{\"contenu\" : \""+msg.getContenu() + "\", \"idMembre\" : \""+msg.getIdMembre()
+						+ "\", \"idEvent\" : \""+msg.getIdEvent()+"\", \"date\" : \""+msg.getDate()+"\"}";
+			}
+			json += "]";
+			out.write(json);
+			out.close();
+		} else {
+			
+			List<Message> messagesClones = this.getListeMessagesMongoDB();
+			
+			response.setContentType("application/json; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			String json = "[";
+			for (Message msg : messagesClones) {
+				json += "\"{\"contenu\" : \""+msg.getContenu() + "\", \"idMembre\" : \""+msg.getIdMembre()
+						+ "\", \"idEvent\" : \""+msg.getIdEvent()+"\", \"date\" : \""+msg.getDate()+"\"}";
+			}
+			json += "]";
+			out.write(json);
+			out.close();
 		}
 		
 	}
+	
+	
+	// --------------
+	//
+	// DO - POST
+	//
+	// --------------
+	
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		doGet(request, response);
+		//doGet(request, response);
+		
+		// Récupérer les paramètres de la requête
+	    String contenuMessage = request.getParameter("contenu");
+	    String idMembre = request.getParameter("userId");
+	    String idEvent = request.getParameter("eventId");
+	    
+	    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	    Date date = new Date();
+	    String datePublication = dateFormat.format(date);
+
+	    if (contenuMessage != null && idMembre != null && idEvent != null) {
+	        insererMessageDansMongoDB(contenuMessage, Integer.parseInt(idMembre), Integer.parseInt(idEvent), datePublication);
+	        
+	    }
 	}
+	
+	
+	private boolean insererMessageDansMongoDB(String contenuMessage, int idMembre, int idEvent, String datePublication) {
+
+	    CodecProvider pojoCodecProvider = PojoCodecProvider.builder().automatic(true).build();
+	    CodecRegistry pojoCodecRegistry = fromRegistries(getDefaultCodecRegistry(), fromProviders(pojoCodecProvider));
+	    ConnectionString connectionString = new ConnectionString("mongodb://obiwan.univ-brest.fr:27017");
+	    MongoClient mongoClient = MongoClients.create(connectionString);
+	    MongoDatabase database = mongoClient.getDatabase("ygasc").withCodecRegistry(pojoCodecRegistry);
+
+
+	    MongoCollection<Message> mess = database.getCollection("messages", Message.class);
+
+	    Message nouveauMessage = new Message();
+	    nouveauMessage.setContenu(contenuMessage);
+	    nouveauMessage.setIdMembre(idMembre);
+	    nouveauMessage.setIdEvent(idEvent);
+	    nouveauMessage.setDate(datePublication);
+
+	    mess.insertOne(nouveauMessage);
+	    mongoClient.close();
+	    return true;
+	}
+
 
 }
--- gestionMessages/src/main/webapp/afficheMessagesMongoDB.jsp
+++
@@ -1,4 +0,0 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-[<c:forEach items="${requestScope.messages}" var="sp">{"contenu" : "${sp.contenu}", "idMembre" : "${sp.idMembre}", "idEvent" : "${sp.idEvent}", "Date" : "${sp.date}"},</c:forEach>]
