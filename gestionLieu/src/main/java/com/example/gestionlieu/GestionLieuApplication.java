c10b6dc90d814e4dc3b707b2ce0d20f6646b6c5a
BastienTLC <bastientalec@orange.fr>
2024-02-26 14:47:48 +0100

    initialisation lieu

---
+++ gestionLieu/src/main/java/com/example/gestionlieu/GestionLieuApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionlieu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionLieuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionLieuApplication.class, args);
+	}
+
+}
